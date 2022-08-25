98103051aa357294caec50c965b033c8cd41e75b
Timothy Graupmann <tgraupmann@gmail.com>
2022-08-25 10:07:03 -0700

    Run UI in UI thread

--- UwpLauncher/LauncherMainPage.xaml.cs
+++ UwpLauncher/LauncherMainPage.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading;
 using System.Threading.Tasks;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
@@ -28,34 +29,58 @@ namespace UwpLauncher
             this.InitializeComponent();
         }
 
-        private async void Page_Loaded(object sender, RoutedEventArgs e)
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            _ = LaunchProcess();
+        }        
+
+        private async Task LaunchProcess()
         {
             for (uint i = 3; i > 0; --i)
             {
-                LblCountdown.Text = String.Format("In {0} seconds...", i);
+                await Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () =>
+                {
+                    LblCountdown.Text = String.Format("In {0} seconds...", i);
+                });
                 await Task.Delay(1000);
             }
 
-            LblCountdown.Text = "Now!";
+            await Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () =>
+            {
+                LblCountdown.Text = "Now!";
+            });
+            
             await Task.Delay(1000);
 
             try
             {
                 if (Windows.Foundation.Metadata.ApiInformation.IsApiContractPresent("Windows.ApplicationModel.FullTrustAppContract", 1, 0))
                 {
-                    await Windows.ApplicationModel.FullTrustProcessLauncher.LaunchFullTrustProcessForAppAsync("Game", "Background");
-                    LblCountdown.Text = "Launch Success!";
+                    await Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, async () =>
+                    {
+                        await Windows.ApplicationModel.FullTrustProcessLauncher.LaunchFullTrustProcessForAppAsync("Game", "Background");
+                    });
+                    await Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () =>
+                    {
+                        LblCountdown.Text = "Launch Success!";
+                    });
                     await Task.Delay(2000);
-                    //Application.Current.Exit();
+                    Application.Current.Exit();
                 }
                 else
                 {
-                    LblCountdown.Text = "Launch Failed! FullTrustAppContract";
+                    await Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () =>
+                    {
+                        LblCountdown.Text = "Launch Failed! FullTrustAppContract";
+                    });
                 }
             }
             catch (Exception ex)
             {
-                LblCountdown.Text = String.Format("Launch Failed! Exception: {0}", ex);
+                await Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () =>
+                {
+                    LblCountdown.Text = String.Format("Launch Failed! Exception: {0}", ex);
+                });
             }            
         }        
     }
